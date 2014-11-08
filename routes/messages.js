4f713f44abbf8f3095fac7c2b1c42c7565078543
Kevin Li <kli@gilt.com>
2014-11-08 18:47:13 -0500

    Adding message model

--- routes/messages.js
+++ routes/messages.js
@@ -1,24 +1,31 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+var Message = require('../models/message');
 var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
-var ObjectId = mongoose.Schema.Types.ObjectId;
-
-var MessageSchema = new Schema({
-    type: String,
-    user: String,
-    item: ObjectId,
-    group: ObjectId,
-    created_at: Date
-});
-
-var Message = mongoose.model('Message', MessageSchema);
 
 router.post('/auth/messages', function(req, res) {
-    res.status(200).end();
+  var messageType = req.body.message_type,
+    username = req.body.user.username,
+    item = req.body.item,
+    group = req.body.group;
+
+  if(messageType === null || messageType === undefined || !(message == 'ASK' || message == 'PROMISE')) {
+    return res.status(500).json("Must supply message_type");
+  }
+
+  if(req.body.item == null || req.body.item === undefined) {
+    return res.status(500).json("Must supply an item");
+  }
+
+  if(req.body.group == null || req.body.group == undefined) {
+    return res.status(500).json("Must supply a group id");
+  }
+
+  var message = new Message(messageType, username, item, group, new Date());
+  message.save();
+
+  res.status(200).end();
 });
 
 module.exports = router;
